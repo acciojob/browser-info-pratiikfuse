f278dfcd9ac5e006efcc7c119b9f99bf3b54e59b
Pratik Fuse <pratikfuse20@gmail.com>
2023-09-15 14:09:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const appName = window.navigator.appName;
+const appVersion = window.navigator.appVersion;
+
+const div = document.createElement('div');
+
+div.id  = "browser-info";
+
+let str  = `You are using ${appName} version ${appVersion}.`;
+
+div.innerHTML = str;
